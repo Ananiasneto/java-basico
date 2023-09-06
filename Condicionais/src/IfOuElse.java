d10d3efbbcfe6453f1e16b5c6ae2c6879f0b1ceb
nias <netimcunha@gmail.com>
2023-09-06 01:56:31 -0300

    Primeiros exercicios de condicionais

---
+++ Condicionais/src/IfOuElse.java
@@ -0,0 +1,12 @@
+public class IfOuElse {
+    public static void main(String[] args) {
+       double saldo=25.0;
+       double valorSolicitado=27.0;
+       if (valorSolicitado<saldo){
+        saldo=saldo-valorSolicitado;
+       }else{
+           System.out.println("saldo insuficiente");
+       }
+        System.out.println(saldo);
+    }
+}
---
+++ Condicionais/src/ResultadoEscola.java
@@ -0,0 +1,10 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+       int nota=8;
+       if (nota<=6){
+        System.out.println("reprovado");
+       }else{
+           System.out.println("aprovado");
+       }
+    }
+}
